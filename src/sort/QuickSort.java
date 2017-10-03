ae3aedff9bd22991ca1321a5d9ca7cb410e7b005
Salvas <Salvas@DESKTOP-AOTBVJ4.home>
2017-10-03 23:10:27 +0100

    QuickSort

---
+++ src/sort/QuickSort.java
@@ -0,0 +1,11 @@
+package sort;
+
+import eds.DLList;
+import eds.InvalidPositionException;
+import home.Home;
+
+public interface QuickSort {
+
+	void sortScore(DLList<Home> homes) throws InvalidPositionException;
+
+}
---
+++ src/sort/QuickSortClass.java
@@ -0,0 +1,51 @@
+package sort;
+
+import eds.*;
+import home.*;
+
+public class QuickSortClass implements QuickSort {
+
+	private DLList<Home> tempList;
+	private int size;
+
+	public void sortScore(DLList<Home> homes) throws InvalidPositionException {
+		if (homes == null || homes.getSize() == 0) {
+			return;
+		}
+		tempList = homes;
+		size = homes.getSize();
+		sort(0, size - 1);
+	}
+
+	private void sort(int start, int end) throws InvalidPositionException {
+		int l = start;
+		int h = end;
+
+		int pivot = tempList.get((l + (h - l)) / 2).getScore();
+
+		while (l <= h) {
+			while (tempList.get(l).getScore() < pivot) {
+				l++;
+			}
+			while (tempList.get(h).getScore() > pivot) {
+				h--;
+			}
+			if (l <= h) {
+				swap(l, h);
+				l++;
+				h--;
+			}
+		}
+		if (start < l)
+			sort(start, l);
+		if (h < end)
+			sort(h, end);
+	}
+
+	private void swap(int i, int k) {
+		Home temp = tempList.get(i);
+		tempList.get(i) = tempList.get(k);
+		tempList.get(k) = temp;
+	}
+
+}
